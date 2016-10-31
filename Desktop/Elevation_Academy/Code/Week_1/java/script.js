81bab785edacbe865f4ad1d7346eed3c97707350
Highbury1913 <wilensky.jason13@gmail.com>
2016-10-31 11:14:32 +0200

    my football match

--- Desktop/Elevation_Academy/Code/Week_1/java/script.js
+++ Desktop/Elevation_Academy/Code/Week_1/java/script.js
@@ -1,2 +1,17 @@
-var frontEnd =['HTML', 'CSS', 'Javascript'];
-var frontEndLangages = frontEnd;
+var home = (3);
+var away = (4);
+var win = ("yaaaaaaaaay")
+var lose =("noooooooooo")
+var draw =("well its not a loss")
+if (home > away) {
+	alert (win);
+
+} else if(home === away){
+	alert (draw)
+}
+else {
+	alert (lose)
+}
+
+
+
